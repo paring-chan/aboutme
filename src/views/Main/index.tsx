3c9f0cb665c77acf24c7a5452351361f075f7b1d
pikokr <parangee9706@gmail.com>
2021-07-27 15:06:29 +0900

    first section color

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactFullpage from '@fullpage/react-fullpage'
+import styles from '../../styles/main.module.scss'
+import clsx from 'clsx'
 
 const MainPage = () => {
     return (
@@ -8,7 +10,7 @@ const MainPage = () => {
             licenseKey="7A6BD9E1-8D334350-80A4BC34-64E1097D"
             render={() => (
                 <div id="fullpage-wrapper">
-                    <div className="section">1</div>
+                    <div className={clsx('section', styles.section1)}>1</div>
                     <div className="section">2</div>
                     <div className="section">3</div>
                     <div className="section">4</div>
