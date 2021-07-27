a6b983cfb3003f73fe2d9cdd9aae689eed7f8eb7
pikokr <parangee9706@gmail.com>
2021-07-27 17:43:36 +0900

    백업

--- src/views/Main/Section2.tsx
+++ src/views/Main/Section2.tsx
@@ -16,16 +16,33 @@ const Section2 = () => {
                 <h1>SKILLS</h1>
                 <div className={styles.skillsGrid}>
                     <Skill
-                        title="TYPESCRIPT"
-                        content="파랑이가 제일 많이 쓰는 언어"
+                        title="JAVASCRIPT/TYPESCRIPT"
+                        content="파랑이가 제일 많이 쓰는 언어에요!"
                     />
                     <Skill
-                        title="TYPESCRIPT"
-                        content="파랑이가 제일 많이 쓰는 언어"
+                        title="Kotlin"
+                        content="파랑이가 제일 좋아하는 언어에요!(사실상 많이 안씀)"
+                    />
+                    <Skill title="Python" content="한번씩(?) 쓰는거에요!" />
+                    <Skill
+                        title="Unity"
+                        content="파랑이가 유일하게 할줄 아는 게임 엔진이애오!"
                     />
                     <Skill
-                        title="TYPESCRIPT"
-                        content="파랑이가 제일 많이 쓰는 언어"
+                        title="React/Next.js"
+                        content="파랑이가 제일 많이 쓰는 웹 프레임워크!"
+                    />
+                    <Skill
+                        title="Vue/Nuxt.js"
+                        content="배워뒀지만 파랑이가 잘 쓰지는 않는 웹 프레임워크!"
+                    />
+                    <Skill
+                        title="Phaser 3"
+                        content="잘은 모르지만 일단 써본거!"
+                    />
+                    <Skill
+                        title="C#"
+                        content="얼불춤 모딩하면서 배운 언어에요!"
                     />
                 </div>
             </div>
