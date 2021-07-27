f6a57c1c7c1a868509fa8dad002ae08e2bd4540e
pikokr <parangee9706@gmail.com>
2021-07-27 15:09:18 +0900

    section background colors

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -10,10 +10,33 @@ const MainPage = () => {
             licenseKey="7A6BD9E1-8D334350-80A4BC34-64E1097D"
             render={() => (
                 <div id="fullpage-wrapper">
-                    <div className={clsx('section', styles.section1)}>1</div>
-                    <div className="section">2</div>
-                    <div className="section">3</div>
-                    <div className="section">4</div>
+                    <div
+                        className={clsx(
+                            'section',
+                            styles.section1,
+                            styles.section,
+                        )}
+                    >
+                        1
+                    </div>
+                    <div
+                        className={clsx(
+                            'section',
+                            styles.section2,
+                            styles.section,
+                        )}
+                    >
+                        2
+                    </div>
+                    <div
+                        className={clsx(
+                            'section',
+                            styles.section3,
+                            styles.section,
+                        )}
+                    >
+                        3
+                    </div>
                 </div>
             )}
         >
