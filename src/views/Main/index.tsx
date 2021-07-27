2e9394994c786fe30299903405d7e95a0090d76d
pikokr <parangee9706@gmail.com>
2021-07-27 16:27:27 +0900

    main section

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactFullpage from '@fullpage/react-fullpage'
 import styles from '../../styles/main.module.scss'
 import clsx from 'clsx'
+import ProfileImage from '../../assets/images/profile.png'
 
 const MainPage = () => {
     return (
@@ -17,7 +18,16 @@ const MainPage = () => {
                             styles.section,
                         )}
                     >
-                        1
+                        <img
+                            src={ProfileImage}
+                            alt="profile"
+                            width={128}
+                            style={{ borderRadius: '50%' }}
+                        />
+                        <h1 style={{ fontSize: 48 }}>파랑이</h1>
+                        <div style={{ fontSize: 24 }}>
+                            진짜 개발*만* 할줄 아는 파랑이애오!
+                        </div>
                     </div>
                     <div
                         className={clsx(
