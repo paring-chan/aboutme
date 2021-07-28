44c38e4406da6d1853e682e20dcff0aff2c4fbae
파랑이 <parangee9706@gmail.com>
2021-07-28 18:41:34 +0900

    intro section

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import 'swiper/swiper.scss'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Mousewheel } from 'swiper/core'
+import Intro from './sections/Intro'
 
 SwiperCore.use([Mousewheel])
 
 const App = () => {
     return (
         <Swiper mousewheel direction="vertical" style={{ height: '100vh' }}>
-            <SwiperSlide>1</SwiperSlide>
-            <SwiperSlide>2</SwiperSlide>
-            <SwiperSlide>3</SwiperSlide>
-            <SwiperSlide>4</SwiperSlide>
+            <SwiperSlide>
+                <Intro />
+            </SwiperSlide>
         </Swiper>
     )
 }
---
+++ src/sections/Intro.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from '../styles/sections/intro.module.scss'
+import profile from '../assets/profile.png'
+
+const Intro = () => {
+    return (
+        <div className={styles.container}>
+            <img
+                src={profile}
+                className={styles.img}
+                width={150}
+                height={150}
+                alt="profile"
+            />
+            <h1>파랑이</h1>
+            <h4>와아! 파랑이애오!</h4>
+        </div>
+    )
+}
+
+export default Intro
