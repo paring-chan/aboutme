fada5b0781cdd0e577d58ad7fa41ac5fdd885e54
pikokr <parangee9706@gmail.com>
2021-07-28 01:15:06 +0000

    Deploying to gh-pages from @ pikokr/aboutme@38ce6d1a91975b95372e80f2a904fe36197913ae 🚀

--- dist/views/Main/index.js
+++ dist/views/Main/index.js
@@ -3,6 +3,7 @@ import ReactFullpage from "../../../_snowpack/pkg/@fullpage/react-fullpage.js";
 import styles from "../../styles/main.module.css.proxy.js";
 import clsx from "../../../_snowpack/pkg/clsx.js";
 import Section1 from "./Section1.js";
+import Section3 from "./Section3.js";
 import Section2 from "./Section2.js";
 const MainPage = () => {
   return /* @__PURE__ */ React.createElement(ReactFullpage, {
@@ -10,7 +11,7 @@ const MainPage = () => {
     licenseKey: "7A6BD9E1-8D334350-80A4BC34-64E1097D",
     render: () => /* @__PURE__ */ React.createElement("div", {
       id: "fullpage-wrapper"
-    }, /* @__PURE__ */ React.createElement(Section1, null), /* @__PURE__ */ React.createElement(Section2, null), /* @__PURE__ */ React.createElement("div", {
+    }, /* @__PURE__ */ React.createElement(Section1, null), /* @__PURE__ */ React.createElement(Section2, null), /* @__PURE__ */ React.createElement(Section3, null), /* @__PURE__ */ React.createElement("div", {
       className: clsx("section", styles.section3, styles.section)
     }, "3"))
   }, "와!");
