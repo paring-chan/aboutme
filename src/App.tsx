644ae788b5555c47d6974e399c9c4bba817992ec
파랑이 <parangee9706@gmail.com>
2021-07-28 18:02:20 +0900

    init swiper js

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import 'swiper/swiper.scss'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Mousewheel } from 'swiper/core'
+
+SwiperCore.use([Mousewheel])
 
 const App = () => {
-    return <div>asdf</div>
+    return (
+        <Swiper mousewheel direction="vertical" style={{ height: '100vh' }}>
+            <SwiperSlide>1</SwiperSlide>
+            <SwiperSlide>2</SwiperSlide>
+            <SwiperSlide>3</SwiperSlide>
+            <SwiperSlide>4</SwiperSlide>
+        </Swiper>
+    )
 }
 
 export default App
