14f1a10a4b75988f9cdfdc3c647bf4337216a89e
파랑이 <parangee9706@gmail.com>
2021-07-31 10:18:21 +0900

    project title, brief, description

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 type ProjectProps = {
     title: string
+    brief: string
+    image: string
     bg?: string
 }
 
-const Project: React.FC<ProjectProps> = ({ bg }) => {
+const Project: React.FC<ProjectProps> = ({ bg, title, brief, image }) => {
     return (
         <div style={{ position: 'relative', width: '100%', height: '100%' }}>
             <div
@@ -18,6 +20,7 @@ const Project: React.FC<ProjectProps> = ({ bg }) => {
                     position: 'absolute',
                     left: 0,
                     top: 0,
+                    zIndex: 10,
                 }}
             />
             <div
@@ -27,8 +30,34 @@ const Project: React.FC<ProjectProps> = ({ bg }) => {
                     position: 'absolute',
                     width: '100%',
                     height: '100%',
+                    zIndex: 11,
                 }}
             />
+            <div
+                style={{
+                    zIndex: 12,
+                    position: 'absolute',
+                    width: '100%',
+                    height: '100%',
+                    top: 0,
+                    left: 0,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'column',
+                    gap: 10,
+                }}
+            >
+                <img
+                    src={image}
+                    width={300}
+                    height={300}
+                    style={{ borderRadius: 10 }}
+                    alt={title}
+                />
+                <h1 style={{ fontSize: 55 }}>{title}</h1>
+                <div style={{ fontSize: 30 }}>{brief}</div>
+            </div>
         </div>
     )
 }
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -12,11 +12,14 @@ const Projects = () => {
                     style={{
                         height: '100%',
                     }}
+                    pagination
                 >
                     <SwiperSlide>
                         <Project
                             bg="https://media.discordapp.net/attachments/845261659336605717/870673796552601660/unknown.png?width=1049&height=590"
                             title="UniqueBots"
+                            brief="디스코드 봇 리스트"
+                            image="https://cdn.discordapp.com/attachments/761800911604940801/798431471362113556/image0.png"
                         />
                     </SwiperSlide>
                     <SwiperSlide>2</SwiperSlide>
