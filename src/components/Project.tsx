93eb21650fb1cc8ff8eb2d192cc5526e9b940d5f
파랑이 <parangee9706@gmail.com>
2021-07-31 10:33:05 +0900

    feat: make image optional

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 type ProjectProps = {
     title: string
     brief: string
-    image: string
+    image?: string
     bg?: string
 }
 
@@ -48,13 +48,15 @@ const Project: React.FC<ProjectProps> = ({ bg, title, brief, image }) => {
                     gap: 10,
                 }}
             >
-                <img
-                    src={image}
-                    width={300}
-                    height={300}
-                    style={{ borderRadius: 10 }}
-                    alt={title}
-                />
+                {image && (
+                    <img
+                        src={image}
+                        width={300}
+                        height={300}
+                        style={{ borderRadius: 10 }}
+                        alt={title}
+                    />
+                )}
                 <h1 style={{ fontSize: 55 }}>{title}</h1>
                 <div style={{ fontSize: 30 }}>{brief}</div>
             </div>
