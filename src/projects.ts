36efb251424a45bb6ea5f8bb3f2abcfa21630102
pikokr <parangee9706@gmail.com>
2021-11-03 16:17:25 +0900

    project card

---
+++ src/projects.ts
@@ -0,0 +1,31 @@
+export type ProjectItem = {
+    title: string
+    description: string
+}
+
+export const projects: ProjectItem[] = [
+    {
+        title: 'Command.TS',
+        description: 'Command Framework Written in Typescript',
+    },
+    {
+        title: 'Command.TS',
+        description: 'Command Framework Written in Typescript',
+    },
+    {
+        title: 'Command.TS',
+        description: 'Command Framework Written in Typescript',
+    },
+    {
+        title: 'Command.TS',
+        description: 'Command Framework Written in Typescript',
+    },
+    {
+        title: 'Command.TS',
+        description: 'Command Framework Written in Typescript',
+    },
+    {
+        title: 'Command.TS',
+        description: 'Command Framework Written in Typescript',
+    },
+]
--- src/views/Projects.tsx
+++ src/views/Projects.tsx
@@ -1,7 +1,8 @@
-import { motion } from 'framer-motion'
+import { AnimateSharedLayout, motion, Variants } from 'framer-motion'
 import React from 'react'
 import styled from 'styled-components'
 import SectionTitle from '../components/SectionTitle'
+import { ProjectItem, projects } from '../projects'
 
 const Container = styled(motion.div)`
     display: flex;
@@ -12,10 +13,83 @@ const Container = styled(motion.div)`
     gap: 30px;
 `
 
+const ProjectContainer = styled(motion.div)`
+    background: #fff;
+    color: #000;
+    padding: 20px;
+    border-radius: 20px;
+`
+
+const ProjectTitle = styled(motion.div)`
+    font-size: 24px;
+    font-weight: bold;
+`
+
+const ProjectDescription = styled(motion.div)`
+    font-size: 16px;
+`
+
+const itemVariants = {
+    hidden: { y: 20, opacity: 0 },
+    show: { y: 0, opacity: 1 },
+    exit: { y: -20, opacity: 0 },
+}
+
+const ProjectCard: React.FC<{
+    id: string
+    item: ProjectItem
+}> = ({ id, item }) => {
+    return (
+        <ProjectContainer variants={itemVariants} layoutId={id}>
+            <ProjectTitle>{item.title}</ProjectTitle>
+            <ProjectDescription>{item.description}</ProjectDescription>
+        </ProjectContainer>
+    )
+}
+
+const ItemsContainer = styled(motion.div)`
+    display: grid;
+    gap: 30px;
+    grid-template-columns: 1fr;
+    @media (min-width: 768px) {
+        grid-template-columns: 1fr 1fr;
+    }
+    @media (min-width: 1024px) {
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+`
+
+const projectsContainerVariants: Variants = {
+    hidden: {},
+    show: {
+        transition: {
+            staggerChildren: 0.05,
+            delayChildren: 0.5,
+        },
+    },
+    exit: {
+        transition: {
+            staggerChildren: 0.05,
+        },
+    },
+}
+
 const Projects: React.FC = () => {
     return (
         <Container>
             <SectionTitle t="Projects" />
+            <AnimateSharedLayout type="crossfade">
+                <ItemsContainer
+                    variants={projectsContainerVariants}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                >
+                    {projects.map((x, i) => (
+                        <ProjectCard item={x} key={i} id={i.toString()} />
+                    ))}
+                </ItemsContainer>
+            </AnimateSharedLayout>
         </Container>
     )
 }
